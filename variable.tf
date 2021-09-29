62db5cc92604230b780bf8a78150d5b6601153b9
tftesting <90293937+tftesting@users.noreply.github.com>
2021-09-29 16:00:08 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -29,3 +29,29 @@ variable "cleintSecret" {
 variable "tenantID" {
   type = string
 }
+
+// K8s Cluster
+variable "resource_group_name" {
+  type        = string
+  description = "RG name in Azure"
+}
+
+variable "cluster_name" {
+  type        = string
+  description = "AKS name in Azure"
+}
+
+variable "kubernetes_version" {
+  type        = string
+  description = "Kubernetes version"
+}
+
+variable "system_node_count" {
+  type        = number
+  description = "Number of AKS worker nodes"
+}
+
+variable "node_resource_group" {
+  type        = string
+  description = "RG name for cluster resources in Azure"
+}
