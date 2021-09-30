5dbc587ceec65eb6906dc3103d7e916caa006e26
tftesting <90293937+tftesting@users.noreply.github.com>
2021-09-30 12:35:55 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -64,3 +64,18 @@ resource "azurerm_kubernetes_cluster" "aks" {
 		Market = "Atlanta"
 	}
 }
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  sku                 = "Premium"
+  admin_enabled       = false
+	
+	tags = {
+		Name = "Loganayaki Kuppusamy"
+		Manager = "Justin Robins"
+		Project = "Home Depot - Sakura"
+		Market = "Atlanta"
+	}
+}
