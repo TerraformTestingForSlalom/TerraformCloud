26b43b83373bd1a58a13b73ef48f2b9e9f26745f
tftesting <90293937+tftesting@users.noreply.github.com>
2021-09-30 08:48:20 -0400

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,11 +1,3 @@
-//variable "virtual_machine_extension_storage_account" {
-  //type = map(object({ endpoint = string, key = string }))
-//}
-
-//variable "vistual_machine_windows_join_domain_secret" {
-  //type = string
-//}
-
 variable "RGName" {
   type = string
 }
